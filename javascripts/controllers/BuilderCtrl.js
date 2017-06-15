929259f65fc1a7e2a4a35ea370cdfe97a740e390
Anessa Ortner <anessa.ortner@gmail.com>
2017-06-15 16:53:22 -0500

    working layer posting to firebase

--- javascripts/controllers/BuilderCtrl.js
+++ javascripts/controllers/BuilderCtrl.js
@@ -7,8 +7,8 @@ app.controller("BuilderCtrl", function($location, $rootScope, $routeParams, $sco
 	$scope.textInput = "";
 	$scope.newEvent = {};
 	$scope.currentLayers = [];
-	$scope.showLayerOptions = true;//SET TO FALSE WHEN READY TO IMPLEMENT IF KEEPING THIS PROCESS
-	// let thisEvent = "";
+	$scope.showLayerOptions = false;
+	let thisEvent = "";
 
 	let layerSelectedImage = "";
 	
@@ -21,7 +21,6 @@ app.controller("BuilderCtrl", function($location, $rootScope, $routeParams, $sco
 	  });
 	};
 
-	getItems(); //REMOVE THIS LINE IF IMPLEMENTING THE SET EVENT FUNCTIONALITY FIRST
 //**************************
 // IMAGE BUILDER V2
 //**************************
@@ -210,83 +209,66 @@ app.controller("BuilderCtrl", function($location, $rootScope, $routeParams, $sco
 	};
 
 	//**************************
-	// SET EVENT
+	// FINAL DESIGN CHANGE
 	//**************************
+	let myEvent = "none"; //adjust this when ready to work out UX on events
 
-  // $scope.saveEvent = () => {
-  // 	$scope.newEvent.uid = $rootScope.user.uid;
-  // 	EventFactory.postNewEvent($scope.newEvent).then((answer) => {
-  // 		$scope.showLayerOptions = true;
-  // 		getItems();
-  // 	}).catch((error) => {
-  // 		console.log(error);
-  // 	});
-  // };
+	$scope.saveMyDesign = () => {
+		let designBase64 = canvas.toDataURL();
+		let newDesign = {
+  		uid : $rootScope.user.uid,
+  		base64code : designBase64,
+  		category : myEvent
+  	};
+  	
+  	
+  	InviteFactory.addInvite(newDesign).then((results) => {
+  			$scope.currentLayers.forEach((layerObj) => {
+  				let newLayerObj = {};
+  				newLayerObj.inviteId = results.data.name;
+  				if (layerObj.string !== undefined){
+  						newLayerObj = {
+  							inviteid: results.data.name,
+								string: layerObj.string,
+								xAxis: layerObj.xAxis,
+								yAxis: layerObj.yAxis,
+								size: layerObj.size,
+								fontType: layerObj.fontType,
+								layernumber: layerObj.layerCounter
+							};
+						} else {
+							newLayerObj = {
+								inviteid: results.data.name,
+								imageCode: layerObj.imageCode,
+								xAxis: layerObj.xAxis,
+								yAxis: layerObj.yAxis,
+								scale: layerObj.scale,
+								layernumber: layerObj.layerCounter
+							};
+						}
+					InviteFactory.createlayerObj(newLayerObj).then(() => {
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+				});
+  	}).catch((error) => {
+  		console.log(error);
+  	});
+	};
 
-	// //**************************
-	// // IMAGE BUILDER
-	// //**************************
+	// **************************
+	// SET EVENT
+	// **************************
 
-	// $scope.newLayer = "";
-	// $scope.textLayerShow = false;
-	// $scope.imageLayerShow = false;
-	// $scope.layer = {};
-
-	// $scope.showTextLayer = () => {
-	// 	$scope.imageLayerShow = false;
-	// 	$scope.textLayerShow = true;
-	// };
-
-	// $scope.showImageLayer = () => {
-	// 	$scope.textLayerShow = false;
-	// 	$scope.imageLayerShow = true;
-	// };
-
-
-	// 	let canvas = document.getElementById("inviteBuilder");
-	// 	let ctx = canvas.getContext('2d');
-
-	// 	let counter = 50;
-
-	// 	let bkgImageObj = {};
-		
-	// 	$scope.textlayers = {
-	// 		line1: "Come celebrate!",
-	// 		line2: "Elizabeth is turning 30",
-	// 		line3: "at M.L.Rose Bar and Grill",
-	// 		line4: "March 30th"
-	// 	};
-
-	// 	let createText = (inputInfo) => {
- //  		ctx.font = "20px Arial";
- //  		ctx.fillText(inputInfo, 10, counter);
- //  		counter = counter + 60;
-	// 	};
-
-	// 	let imageMaker = (newImageObj) => {
-	// 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// 		counter = 50;
-	// 		let image = new Image();
-	// 		image.onload = () => {
-	// 			ctx.drawImage(image, 0, 0, image.width * 0.5, image.height * 0.5);
-	// 			//SET TEXT HERE:
-	// 			createText($scope.textlayers.line1);
-	// 			createText($scope.textlayers.line2);
-	// 			createText($scope.textlayers.line3);
-	// 			createText($scope.textlayers.line4);
-	// 		};
-	// 		image.src = `data:${newImageObj.filetype};base64,${newImageObj.base64code}`;
-	// 	};
-
-	// $scope.setMyImage = (imageObj) => {
-	// 	bkgImageObj = imageObj;
-	// 	imageMaker(imageObj);
-	// };
-
-	// $scope.updateCanvas = () => {
-	// 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// 	imageMaker(bkgImageObj);
-	// 	counter = 50;
-	// };
+  $scope.saveEvent = () => {
+  	$scope.newEvent.uid = $rootScope.user.uid;
+  	EventFactory.postNewEvent($scope.newEvent).then((answer) => {
+  		$scope.showLayerOptions = true;
+  		getItems();
+  	}).catch((error) => {
+  		console.log(error);
+  	});
+  };
 
 });
--- javascripts/factories/InviteFactory.js
+++ javascripts/factories/InviteFactory.js
@@ -1,7 +1,25 @@
 app.factory("InviteFactory", function($q, $http, $rootScope, FIREBASE_CONFIG) {
-	let addInvite = () => {
-		console.log("invite factory working");
+	let addInvite = (newInvite) => {
+		return $q((resolve, reject) => {
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/invites.json`, JSON.stringify(newInvite))
+			.then((results) => {
+				resolve(results);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
 	};
 
-	return {addInvite:addInvite};
+	let createlayerObj = (newLayer) => {
+		return $q((resolve, reject) => {
+			$http.post(`${FIREBASE_CONFIG.databaseURL}/layers.json`, JSON.stringify(newLayer))
+			.then((results) => {
+				resolve(results);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	};
+
+	return {addInvite:addInvite, createlayerObj:createlayerObj};
 });
